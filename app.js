f4072773cbe37056e7a1846d824933e0a58c821f
timlo <a0987642936@yahoo.com>
2020-04-14 22:01:35 +0800

    Connect to azure cosmos db

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  mongoose.set('useNewUrlParser', true);
+  mongoose.set('useFindAndModify', false);
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('useUnifiedTopology', true);
+  mongoose.connect(process.env.AZURE_COSMOS_CONNECTION_STRING)
+    .then(() => { console.log('CosmosDB connected') })
+    .catch(err => console.log(err))
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
