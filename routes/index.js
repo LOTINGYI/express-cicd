9ee2ac31377e652fabf5c417554deccab12215ab
timlo <a0987642936@yahoo.com>
2020-04-14 22:48:47 +0800

    Add the model and test whether connect to db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/userModel')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  const user = await User.find()
+  res.render('index', { user });
 });
 
 module.exports = router;
